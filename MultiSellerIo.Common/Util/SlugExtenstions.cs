d52c6f790e59ee1d94e33855452997d20fd7b6ce
Sajith Lakjaya <sajith@develop.lk>
2018-02-21 09:58:30 +0530

    product slug implementation

---
+++ MultiSellerIo.Common/Util/SlugExtenstions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MultiSellerIo.Common.Util
+{
+    public static class SlugExtenstions
+    {
+        //Reference by
+        //https://gist.github.com/joancaron/8436664
+        public static string ToUrlSlug(string value)
+        {
+            //First to lower case 
+            value = value.ToLowerInvariant();
+
+            //Remove all accents
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
+
+            value = Encoding.ASCII.GetString(bytes);
+
+            //Replace spaces 
+            value = Regex.Replace(value, @"\s", "-", RegexOptions.Compiled);
+
+            //Remove invalid chars 
+            value = Regex.Replace(value, @"[^\w\s\p{Pd}]", "", RegexOptions.Compiled);
+
+            //Trim dashes from end 
+            value = value.Trim('-', '_');
+
+            //Replace double occurences of - or \_ 
+            value = Regex.Replace(value, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            return value;
+        }
+    }
+}
